e31567dd22ee5810305687bb1fcf3e01140dabd3
Yunus-S <78805525+Yunus-S@users.noreply.github.com>
2021-03-08 14:20:37 +0100

    Update Labyrinthe.java

--- pacmanFusion/src/Labyrinthe.java
+++ pacmanFusion/src/Labyrinthe.java
@@ -16,15 +16,55 @@ public class Labyrinthe {
             String ligne;
             while((ligne = reader.readLine()) != null){
                 if(ligne.startsWith("{")||ligne.startsWith("}")){
-                    System.out.println(ligne);
+                    //System.out.println(ligne);
                     tableau += ligne + "\n" ;
 
                 }
 
             }
-            System.out.println(tableau);
+            //System.out.println(tableau);
         } catch (Exception ex){
             System.err.println("Error. "+ex.getMessage());
         }
     }
+    
+    public static int[][] convertion(String tab){
+        int[][] lab;
+        lab = new int[21][21];
+
+        //int i = tab.length();
+        //System.out.println(i);
+        int y = 0;
+        int z = 0;
+        for (int x=0; x<tab.length(); x++){
+            //System.out.println(tab.charAt(x));
+            if (tab.charAt(x) == '1'){
+                lab[y][z] = 1;
+                y++;
+            }
+            if (tab.charAt(x) == '0'){
+                lab[y][z] = 0;
+                y++;
+            }
+            if (tab.charAt(x) == '8'){
+                lab[y][z] = 8;
+                y++;
+            }
+            if (tab.charAt(x) == '}'){
+                z++;
+                y=0;
+            }
+
+        }
+
+        //for (int a=0; a<21;a++){
+        //    for (int b=0; b<21; b++){
+        //        System.out.println(lab[b][a]);
+        //    }
+        //}
+
+        return lab;
+    }
+    
+    
 }
