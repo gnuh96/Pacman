83fad01884a1a5f4286825d75392ee20d1d71d0f
SandricCourant <36101178+SandricCourant@users.noreply.github.com>
2021-03-08 15:55:51 +0100

    Update Labyrinthe.java
    Add conversion method of Yunnus S

--- pacmanFusion/src/Labyrinthe.java
+++ pacmanFusion/src/Labyrinthe.java
@@ -4,17 +4,23 @@ import java.io.IOException;
 import java.io.*;
 
 public class Labyrinthe {
-    private String tab;
+
+    private int [][] tab;
     private String path = "resources/Laby.txt";
+
     public Labyrinthe(){//Constructeur
         setTab();//Creer tab
     }
-    public void setTab() {//Init tab
-        this.tab = readFile(this.path);//Lire fichier Laby
+
+    private void setTab() {//Init tab
+        String tableau = readFile(this.path);//Lire fichier Laby
+        this.tab = convertion(tableau);
     }
-    public String getTab(){//Obtenir tab
+
+    public int[][] getTab(){//Obtenir tab
         return this.tab;
-    }
+    }//Obtenir tab
+
     public static String readFile(String pathToFile){//Lire path
         String tableau = "";
 
@@ -32,5 +38,42 @@ public class Labyrinthe {
             System.err.println("Error. "+ex.getMessage());
         }
         return tableau;
+    }
+    public int[][] convertion(String tab){
+        int[][] lab;
+        lab = new int[21][21];
+
+        //int i = tab.length();
+        //System.out.println(i);
+        int y = 0;
+        int z = 0;
+        for (int x=0; x<tab.length(); x++){
+            //System.out.println(tab.charAt(x));
+            if (tab.charAt(x) == '1'){
+                lab[y][z] = 1;
+                y++;
+            }
+            if (tab.charAt(x) == '0'){
+                lab[y][z] = 0;
+                y++;
+            }
+            if (tab.charAt(x) == '8'){
+                lab[y][z] = 8;
+                y++;
+            }
+            if (tab.charAt(x) == '}'){
+                z++;
+                y=0;
+            }
+
+        }
+
+        //for (int a=0; a<21;a++){
+        //    for (int b=0; b<21; b++){
+        //        System.out.println(lab[b][a]);
+        //    }
+        //}
+
+        return lab;
     }
 }
