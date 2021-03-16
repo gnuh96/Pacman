8718560b95ea1d6a6c9b80ff3d8d1078519ec1ca
Yunus-S <78805525+Yunus-S@users.noreply.github.com>
2021-03-16 17:21:49 +0100

    Update PacCompoments.java
    Affichage complet du labyrinthe avec les bon blocs au bon endroit

--- pacmanFusion/src/model/PacCompoments.java
+++ pacmanFusion/src/model/PacCompoments.java
@@ -36,6 +36,182 @@ public class PacCompoments extends JComponent //component class for storing the 
     public void paintComponent(Graphics g) //draws every object
     {
 
+        Image murh = null; //murHorizontal
+        Image murv = null; //murVertical
+        Image angle1 = null;
+        Image angle2 = null;
+        Image angle3 = null;
+        Image angle4 = null;
+        Image gomme = null;
+        Image vide = null;
+        Image croisement =null;
+        Image croiseT = null;
+        Image croiseT2 = null;
+        Image croiseT3 = null;
+        Image croiseT4 = null;
+        Image cds = null;
+        Image cds2 = null;
+        Image cds3 = null;
+        Image cds4 = null;
+
+
+        try {
+            murh = ImageIO.read(new File("src/model/resources/murDroit.png"));
+            murv = ImageIO.read(new File("src/model/resources/murDroit2.png"));
+            angle1 = ImageIO.read(new File("src/model/resources/murAngle.png"));
+            angle2 = ImageIO.read(new File("src/model/resources/murAngle2.png"));
+            angle3 = ImageIO.read(new File("src/model/resources/murAngle3.png"));
+            angle4 = ImageIO.read(new File("src/model/resources/murAngle4.png"));
+            gomme = ImageIO.read(new File("src/model/resources/point.png"));
+            vide = ImageIO.read(new File("src/model/resources/vide.png"));
+            croisement = ImageIO.read(new File("src/model/resources/croisement.png"));
+            croiseT = ImageIO.read(new File("src/model/resources/croiseT.png"));
+            croiseT2 = ImageIO.read(new File("src/model/resources/croiseT2.png"));
+            croiseT3 = ImageIO.read(new File("src/model/resources/croiseT3.png"));
+            croiseT4 = ImageIO.read(new File("src/model/resources/croiseT4.png"));
+            cds = ImageIO.read(new File("src/model/resources/murCulDeSac3.png"));
+            cds2 = ImageIO.read(new File("src/model/resources/murCulDeSac2.png"));
+            cds3 = ImageIO.read(new File("src/model/resources/murCulDeSac.png"));
+            cds4 = ImageIO.read(new File("src/model/resources/murCulDeSac4.png"));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Labyrinthe lb = new Labyrinthe();
+        int[][] tab = lb.getTab();
+        for (int i = 0; i < 21; i++) {
+            for(int j = 0; j < 21; j++) {
+                if (tab[i][j]==1){
+                    if(j==0){
+                        if(i==20){
+                            g.drawImage(angle2, i * 30, j * 30, this);
+                        } else {
+                            if (i==0){
+                                g.drawImage(angle1, i * 30, j * 30, this);
+                            } else {
+                                if (tab[i][j + 1] == 1 & tab[i + 1][j] == 1 & tab[i - 1][j] == 1) {
+                                    g.drawImage(croiseT, i * 30, j * 30, this);
+                                }
+                                if (tab[i][j + 1] != 1) {
+                                    g.drawImage(murh, i * 30, j * 30, this);
+                                }
+                            }
+                        }
+                    }
+                    if(0<j & j<20 & 0<i & i<20) {
+                        if (tab[i][j + 1] == 1 & tab[i][j - 1] == 1) {
+                            g.drawImage(murv, i * 30, j * 30, this);
+                        }
+                        if (tab[i + 1][j] == 1 & tab[i - 1][j] == 1) {
+                            g.drawImage(murh, i*30, j*30, this);
+                        }
+                        if(tab[i][j + 1] == 1 & tab[i][j - 1] == 1 & tab[i + 1][j] == 1){
+                            g.drawImage(croiseT4, i*30, j*30, this);
+                        }
+                        if(tab[i][j + 1] == 1 & tab[i][j - 1] == 1 & tab[i - 1][j] == 1){
+                            g.drawImage(croiseT2, i*30, j*30, this);
+                        }
+                        if(tab[i][j + 1] == 1 & tab[i + 1][j] == 1 & tab[i - 1][j] == 1){
+                            g.drawImage(croiseT, i*30, j*30, this);
+                        }
+                        if(tab[i][j - 1] == 1 & tab[i + 1][j] == 1 & tab[i - 1][j] == 1){
+                            g.drawImage(croiseT3, i*30, j*30, this);
+                        }
+                        if(tab[i][j + 1] == 1 & tab[i][j - 1] == 1 & tab[i - 1][j] == 1 & tab[i + 1][j] == 1){
+                            g.drawImage(croisement, i*30, j*30, this);
+                        }
+                        if(tab[i][j + 1] != 1 & tab[i][j - 1] == 1 & tab[i - 1][j] != 1 & tab[i + 1][j] != 1){
+                            g.drawImage(cds4, i*30, j*30, this);
+                        }
+                        if(tab[i][j + 1] == 1 & tab[i][j - 1] != 1 & tab[i - 1][j] != 1 & tab[i + 1][j] != 1){
+                            g.drawImage(cds3, i*30, j*30, this);
+                        }
+                        if(tab[i][j + 1] != 1 & tab[i][j - 1] != 1 & tab[i - 1][j] == 1 & tab[i + 1][j] != 1){
+                            g.drawImage(cds2, i*30, j*30, this);
+                        }
+                        if(tab[i][j + 1] != 1 & tab[i][j - 1] != 1 & tab[i - 1][j] != 1 & tab[i + 1][j] == 1){
+                            g.drawImage(cds, i*30, j*30, this);
+                        }
+                        if(tab[i][j + 1] == 1 & tab[i][j - 1] != 1 & tab[i - 1][j] != 1 & tab[i + 1][j] == 1){
+                            g.drawImage(angle1, i*30, j*30, this);
+                        }
+                        if(tab[i][j + 1] == 1 & tab[i][j - 1] != 1 & tab[i - 1][j] == 1 & tab[i + 1][j] != 1){
+                            g.drawImage(angle2, i*30, j*30, this);
+                        }
+                        if(tab[i][j + 1] != 1 & tab[i][j - 1] == 1 & tab[i - 1][j] == 1 & tab[i + 1][j] != 1){
+                            g.drawImage(angle4, i*30, j*30, this);
+                        }
+                        if(tab[i][j + 1] != 1 & tab[i][j - 1] == 1 & tab[i - 1][j] != 1 & tab[i + 1][j] == 1){
+                            g.drawImage(angle3, i*30, j*30, this);
+                        }
+                    }
+                    if(0<j & j<20 & i==0){
+                        if(tab[i][j + 1] == 1 & tab[i][j - 1] == 1 & tab[i + 1][j] == 1){
+                            g.drawImage(croiseT4, i*30, j*30, this);
+                        }
+                        if(tab[i][j + 1] == 1 & tab[i][j - 1] != 1 & tab[i + 1][j] == 1){
+                            g.drawImage(angle1, i*30, j*30, this);
+                        }
+                        if(tab[i][j + 1] == 1 & tab[i][j - 1] == 1 & tab[i + 1][j] != 1){
+                            g.drawImage(murv, i*30, j*30, this);
+                        }
+                        if(tab[i][j + 1] != 1 & tab[i][j - 1] == 1 & tab[i + 1][j] == 1){
+                            g.drawImage(angle3, i*30, j*30, this);
+                        }
+                        if(tab[i][j + 1] != 1 & tab[i][j - 1] != 1 & tab[i + 1][j] == 1){
+                            g.drawImage(murh, i*30, j*30, this);
+                        }
+                    }
+                    if(0<j & j<20 & i==20){
+                        if(tab[i][j + 1] == 1 & tab[i][j - 1] == 1 & tab[i - 1][j] == 1){
+                            g.drawImage(croiseT2, i*30, j*30, this);
+                        }
+                        if(tab[i][j + 1] == 1 & tab[i][j - 1] != 1 & tab[i - 1][j] == 1){
+                            g.drawImage(angle2, i*30, j*30, this);
+                        }
+                        if(tab[i][j + 1] == 1 & tab[i][j - 1] == 1 & tab[i - 1][j] != 1){
+                            g.drawImage(murv, i*30, j*30, this);
+                        }
+                        if(tab[i][j + 1] != 1 & tab[i][j - 1] == 1 & tab[i - 1][j] == 1){
+                            g.drawImage(angle4, i*30, j*30, this);
+                        }
+                        if(tab[i][j + 1] != 1 & tab[i][j - 1] != 1 & tab[i - 1][j] == 1){
+                            g.drawImage(murh, i*30, j*30, this);
+                        }
+                    }
+                    if(j==20){
+                        if(i==0){
+                            g.drawImage(angle3, i*30, j*30, this);
+                        } else {
+                            if(i==20){
+                                g.drawImage(angle4, i*30, j*30, this);
+                            } else {
+
+                                if (tab[i + 1][j] == 1 & tab[i - 1][j] == 1) {
+                                    g.drawImage(murh, i * 30, j * 30, this);
+                                }
+                                if (tab[i][j - 1] == 1) {
+                                    g.drawImage(croiseT3, i * 30, j * 30, this);
+                                }
+                            }
+                        }
+                    }
+
+                }
+                if (tab[i][j]==2){
+                    g.drawImage(gomme, i*30, j*30, this);
+                }
+
+                if (tab[i][j]==8){
+                    g.drawImage(vide, i*30, j*30, this);
+                }
+
+            }
+        }
+        
+        
+        
         pac.draw(g); //draws pacman
         //fantome.
         if(stopped) {//draws stop screen
@@ -64,24 +240,6 @@ public class PacCompoments extends JComponent //component class for storing the 
             g.drawString("GAME OVER",(994-200)/2+53,(692-20)/2);
         }
         
-        Image img = null;
-        try {
-            img = ImageIO.read(new File("src/model/resources/murDroit.png"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        Labyrinthe lb = new Labyrinthe();
-        int[][] tab = lb.getTab();
-        for (int i = 0; i < 21; i++) {
-            for(int j = 0; j < 21; j++) {
-                if (tab[i][j]==1){
-                    g.drawImage(img, i*30, j*30, this);
-                }
-
-
-            }
-        }
     }
 
     public void reDraw() //repaints and sets variables dependent on time
